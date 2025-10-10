da457833aa23e7dd3dfe5c7b7dceaad4350c06bb
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-10-10 17:22:44 +0900

    feat: Item 파일 제거 및 App 파일에 Item을 Project로 변경

--- MileStone/MileStoneApp.swift
+++ MileStone/MileStoneApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct MileStoneApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Project.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
