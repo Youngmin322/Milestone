90c106bf30767b7d724491d8a07432902e4b0bce
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-10-10 16:03:17 +0900

    feat: 프로젝트 기본 모델 구성

---
+++ MileStone/Models/Project.swift
@@ -0,0 +1,29 @@
+//
+//  Project.swift
+//  MileStone
+//
+//  Created by Youngmin Cho on 10/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Project {
+    var title: String
+    var projectDescription: String
+    var techStack: [String]
+    var startDate: Date
+    var endDate: Date?
+    var thumbnail: Date?
+    
+    init(title: String, projectDescription: String, techStack: [String], startDate: Date, endDate: Date, thumbnail: Date? = nil) {
+        self.title = title
+        self.projectDescription = projectDescription
+        self.techStack = techStack
+        self.startDate = startDate
+        self.endDate = endDate
+        self.thumbnail = thumbnail
+    }
+}
+
