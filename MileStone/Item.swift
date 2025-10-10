3e1299c1b2e7ffa6291b9c2ead46344510d77b2d
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-10-10 15:33:41 +0900

    프로젝트 파일 생성

---
+++ MileStone/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MileStone
+//
+//  Created by Youngmin Cho on 10/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
