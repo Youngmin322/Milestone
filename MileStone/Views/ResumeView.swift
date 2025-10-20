f637590555840bcdc60850729056a093c45466b4
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-10-20 14:52:47 +0900

    feat: ResumeView 초기 구조 세팅

--- MileStone/Views/ResumeView.swift
+++ MileStone/Views/ResumeView.swift
@@ -5,4 +5,43 @@
 //  Created by Youngmin Cho on 10/20/25.
 //
 
-import Foundation
+import SwiftUI
+import PDFKit
+import UniformTypeIdentifiers
+
+struct ResumeView: View {
+    @State private var pdfDocument: PDFDocument?
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                if let pdfDocument = pdfDocument {
+                    PDFKitView(pdfDocument: pdfDocument)
+                        .ignoresSafeArea()
+                } else {
+                    
+                }
+            }
+        }
+    }
+}
+
+struct PDFKitView: UIViewRepresentable {
+    let pdfDocument: PDFDocument
+    
+    func makeUIView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = pdfDocument
+        pdfView.autoScales = true
+        pdfView.displayMode = .singlePageContinuous
+        pdfView.displayDirection = .vertical
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        uiView.document = pdfDocument
+    }
+}
+
+#Preview {
+    ResumeView()
+}
